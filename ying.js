1e12a1d5a8c571708ab6a5e2cd6c02c1909a7640
Charles <Charles@0x7f.cc>
2021-11-17 04:43:27 +0800

    Bug fix

--- ying.js
+++ ying.js
@@ -1,22 +1,40 @@
-(function(){
-    'use strict';
+(function () {
+  "use strict";
+
+  function regex_replace(base, child) {
     var yin = "陻音阴愔慇瘖磤禋欭殷氤洇姻喑噾因堙骃溵湮濦絪緸茵荫裀諲闉",
-        yin_2 = "霪银夤訚狺峾崟寅婬犾唫嚚圻垠冘吟龈龂淫蟫訔誾鄞",
-        yin_4 = "隐窨慭懚梀廕印胤酳",
-        ying = "韺瑛锳璎撄孾柍樱嫈媖婴啨应鹦鹰瀴缨罂膺莺英荥蘡蠳譍譻賏",
-        ying_2 = "盈禜籯桯楹浧嬴莹滢潆濙濚瀛瀯茔荧萤萦营蝇謍赢迎",
-        ying_4 = "硬映媵",
-        ying_reg = RegExp("(?<=(^|>)[^<]*)[" + ying + ying_2 + ying_4 + "](?=[^>]*)","g"),
-        yin_reg = RegExp("(?<=(^|>)[^<]*)[" + yin + yin_2 + yin_4 + "](?=[^>]*)","g");
-
-    onload = function()
-    {
-        var eles = document.body.innerHTML;
-        eles = eles.replace(ying_reg, "<ying ori=\"$&\" class=\"ying\"></ying>");
-        eles = eles.replace(yin_reg, "<ying ori=\"$&\" class=\"ying\"></ying>");
-        document.body.innerHTML = eles;
+      yin_2 = "霪银夤訚狺峾崟寅婬犾唫嚚圻垠冘吟龈龂淫蟫訔誾鄞",
+      yin_4 = "隐窨慭懚梀廕印胤酳",
+      ying = "韺瑛锳璎撄孾柍樱嫈媖婴啨应鹦鹰瀴缨罂膺莺英荥蘡蠳譍譻賏",
+      ying_2 = "盈禜籯桯楹浧嬴莹滢潆濙濚瀛瀯茔荧萤萦营蝇謍赢迎",
+      ying_4 = "硬映媵",
+      ying_reg = RegExp(
+        "(?<=(^|>)[^<]*)[" + ying + ying_2 + ying_4 + "](?=[^>]*)",
+        "g"
+      ),
+      yin_reg = RegExp(
+        "(?<=(^|>)[^<]*)[" + yin + yin_2 + yin_4 + "](?=[^>]*)",
+        "g"
+      );
+    if (ying_reg.test(child.textContent) || yin_reg.test(child.textContent)) {
+      base.innerHTML = base.innerHTML
+        .replace(ying_reg, '<ying ori="$&" class="ying"></ying>')
+        .replace(yin_reg, '<ying ori="$&" class="ying"></ying>');
     }
-}());
+  }
 
+  function iter(nodes) {
+    for (var i = 0, n = nodes.childNodes.length; i < n; i++) {
+      if (nodes.childNodes[i].nodeType == 3) {
+        regex_replace(nodes, nodes.childNodes[i]);
+      }
+      if (nodes.childNodes[i].childNodes.length != 0) {
+        iter(nodes.childNodes[i]);
+      }
+    }
+  }
 
-
+  onload = function () {
+    iter(document.body);
+  };
+})();
--- ying.user.js
+++ ying.user.js
@@ -1,30 +1,54 @@
 // ==UserScript==
 // @name         YingYingYing
-// @namespace    https://github.com/SteveCharlesYang/ying
-// @version      0.1.4
+// @namespace    https://github.com/YukariChiba/ying
+// @version      0.2.0
 // @description  Replace Yings in Chinese
-// @author       Charles Yang
-// @updateURL    https://raw.githubusercontent.com/SteveCharlesYang/ying/master/ying.user.js
-// @downloadURL  https://raw.githubusercontent.com/SteveCharlesYang/ying/master/ying.user.js
+// @author       Yukari Chiba
+// @updateURL    https://raw.githubusercontent.com/YukariChiba/ying/master/ying.user.js
+// @downloadURL  https://raw.githubusercontent.com/YukariChiba/ying/master/ying.user.js
 // @match        http*://*/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle("ying::after { content: \"嘤\"; } ying:hover::after { content: attr(ori); } ");
+GM_addStyle(
+  'ying::after { content: "嘤"; } ying:hover::after { content: attr(ori); } '
+);
 
-(function(){
-    'use strict';
-    var yin = "陻音阴愔慇瘖磤禋欭殷氤洇姻喑噾因堙骃溵湮濦絪緸茵荫裀諲闉",
-        yin_2 = "霪银夤訚狺峾崟寅婬犾唫嚚圻垠冘吟龈龂淫蟫訔誾鄞",
-        yin_4 = "隐窨慭懚梀廕印胤酳",
-        ying = "韺瑛锳璎撄孾柍樱嫈媖婴啨应鹦鹰瀴缨罂膺莺英荥蘡蠳譍譻賏",
-        ying_2 = "盈禜籯桯楹浧嬴莹滢潆濙濚瀛瀯茔荧萤萦营蝇謍赢迎",
-        ying_4 = "硬映媵",
-        ying_reg = RegExp("(?<=(^|>)[^<]*)[" + ying + ying_2 + ying_4 + "](?=[^>]*)","g"),
-        yin_reg = RegExp("(?<=(^|>)[^<]*)[" + yin + yin_2 + yin_4 + "](?=[^>]*)","g");
+function regex_replace(base, child) {
+  var yin = "陻音阴愔慇瘖磤禋欭殷氤洇姻喑噾因堙骃溵湮濦絪緸茵荫裀諲闉",
+    yin_2 = "霪银夤訚狺峾崟寅婬犾唫嚚圻垠冘吟龈龂淫蟫訔誾鄞",
+    yin_4 = "隐窨慭懚梀廕印胤酳",
+    ying = "韺瑛锳璎撄孾柍樱嫈媖婴啨应鹦鹰瀴缨罂膺莺英荥蘡蠳譍譻賏",
+    ying_2 = "盈禜籯桯楹浧嬴莹滢潆濙濚瀛瀯茔荧萤萦营蝇謍赢迎",
+    ying_4 = "硬映媵",
+    ying_reg = RegExp(
+      "(?<=(^|>)[^<]*)[" + ying + ying_2 + ying_4 + "](?=[^>]*)",
+      "g"
+    ),
+    yin_reg = RegExp(
+      "(?<=(^|>)[^<]*)[" + yin + yin_2 + yin_4 + "](?=[^>]*)",
+      "g"
+    );
+  if (ying_reg.test(child.textContent) || yin_reg.test(child.textContent)) {
+    base.innerHTML = base.innerHTML
+      .replace(ying_reg, '<ying ori="$&" class="ying"></ying>')
+      .replace(yin_reg, '<ying ori="$&" class="ying"></ying>');
+  }
+}
 
-        var eles = document.body.innerHTML;
-        eles = eles.replace(ying_reg, "<ying ori=\"$&\" class=\"ying\"></ying>");
-        eles = eles.replace(yin_reg, "<ying ori=\"$&\" class=\"ying\"></ying>");
-        document.body.innerHTML = eles;
-}());
+function iter(nodes) {
+  for (var i = 0, n = nodes.childNodes.length; i < n; i++) {
+    if (nodes.childNodes[i].nodeType == 3) {
+      regex_replace(nodes, nodes.childNodes[i]);
+    }
+    if (nodes.childNodes[i].childNodes.length != 0) {
+      iter(nodes.childNodes[i]);
+    }
+  }
+}
+
+(function () {
+  "use strict";
+  var eles = document.body;
+  iter(eles);
+})();
